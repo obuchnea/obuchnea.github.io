42f0181b43000e0d086c8d38ff1d7d6cc1a56aca
obuchnea <obuchnea1@gmail.com>
2026-09-14 17:32:30 -0400

    Added animations to page transitions

--- header.js
+++ header.js
@@ -3,15 +3,41 @@ class SiteHeader extends HTMLElement {
   connectedCallback() {
     this.innerHTML = `
       <div class="gallery-header" style="margin-bottom: 5px; text-align: center;">
-        <a href="/index.html" style="margin-right: 50px;">home</a>
+        <a href="/" style="margin-right: 50px;">home</a>
         <span style="margin-right: 50px;">|</span>
-        <a href="/about-me.html" style="margin-right: 50px;">about me</a>
+        <a href="/about-me/" style="margin-right: 50px;">about me</a>
         <span style="margin-right: 50px;">|</span>
-        <a href="/workterms/workterm1.html" style="margin-right: 50px;">workterms</a>
+        <a href="/workterms/workterm1/" style="margin-right: 50px;">workterms</a>
         <span style="margin-right: 50px;">|</span>
-        <a href="/gallery.html">gallery</a>
+        <a href="/gallery/">gallery</a>
       </div>
     `;
   }
 }
 customElements.define('site-header', SiteHeader);
+
+document.addEventListener("DOMContentLoaded", () => {
+    document.querySelectorAll("a").forEach(link => {
+        link.addEventListener("click", e => {
+            const href = link.href;
+
+            // Ignore external links, anchors, new tabs, etc.
+            if (
+                !href ||
+                link.target === "_blank" ||
+                href.startsWith("#") ||
+                new URL(href).origin !== window.location.origin
+            ) {
+                return;
+            }
+
+            e.preventDefault();
+
+            document.body.classList.add("page-exit");
+
+            setTimeout(() => {
+                window.location.href = href;
+            }, 500);
+        });
+    });
+});
