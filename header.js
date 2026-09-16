5c7bac805aa16440aac7bd3123546d2441fc7ffc
obuchnea <obuchnea1@gmail.com>
2026-09-15 20:24:11 -0400

    Added mobile compatability

--- header.js
+++ header.js
@@ -2,13 +2,13 @@
 class SiteHeader extends HTMLElement {
   connectedCallback() {
     this.innerHTML = `
-      <div class="gallery-header" style="margin-bottom: 5px; text-align: center;">
-        <a href="/" style="margin-right: 50px;">home</a>
-        <span style="margin-right: 50px;">|</span>
-        <a href="/about-me/" style="margin-right: 50px;">about me</a>
-        <span style="margin-right: 50px;">|</span>
-        <a href="/workterms/workterm1/" style="margin-right: 50px;">workterms</a>
-        <span style="margin-right: 50px;">|</span>
+      <div class="gallery-header">
+        <a href="/">home</a>
+        <span>|</span>
+        <a href="/about-me/">about me</a>
+        <span>|</span>
+        <a href="/workterms/workterm1/">workterms</a>
+        <span>|</span>
         <a href="/gallery/">gallery</a>
       </div>
     `;
